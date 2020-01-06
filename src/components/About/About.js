a94b581ff8e9c4d970a58ccdc8bb78c8ba0f3cee
Egor Deryabin <egor19942012@gmail.com>
2020-01-06 18:11:20 +0300

    added basic info to About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,20 +1,19 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';	
 import logoWithColor from '../../images/logoWithColor.jpg';
 import './index.css';
 
-export default () => (
-	<div className="about">
-		<img src={logoWithColor} className="about__logo" alt="logo" />
-		<div className="about__description">
-			<h4 className="about__description__title">МОБИЛЬНЫЙ РАЗРАБОТЧИК</h4>
-			<p className="about__description__text">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-				Fusce aliquet, est nec aliquam dictum, quam augue tincidunt odio, 
-				vitae maximus odio dui at nibh. Duis blandit tortor non nisl eleifend maximus. 
-				Quisque iaculis id neque sed interdum. Etiam eu ex ut nulla aliquam finibus 
-				quis id elit. Nam in purus quam. Proin gravida justo id massa cursus euismod.
-				Sed faucibus eros in iaculis tincidunt.
-			</p>
+export default () => {
+	const { t } = useTranslation();
+
+	return (
+		<div className="about">
+			<img src={logoWithColor} className="about__logo" alt="logo" />
+			<div className="about__description">
+				<h4 className="about__description__title">Коротко о себе.</h4>
+				<p className="about__description__text">{t('about.first__text')}</p>
+				<p className="about__description__text">{t('about.second__text')}</p>
+			</div>
 		</div>
-	</div>
-);
+	);
+}
